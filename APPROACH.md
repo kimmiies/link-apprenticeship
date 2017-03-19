ffd639baba6ae17d9f78909bd74e3b47b2b4f8fe
Kimberly Oleiro <kimberly.oleiro@telus.com>
2017-03-19 14:51:56 -0400

    updated readme and added reflection

---
+++ APPROACH.md
@@ -0,0 +1,11 @@
+First Iteration
+===============
+
+Start simple and write a program that works. Using one function and conditional statements to determine the cost of a project given three inputs (base price, number of people required for job and the item category).
+
+Set up test framework Jasmine that tests the program outputs based on the three examples given in the program.
+
+Thinking about:
+- Where and how to validation inputs. In the program or in the test specs?
+- Functional programming vs OOP
+- Writing more meaningful tests
--- spec/NupackSpec.js
+++ spec/NupackSpec.js
@@ -1,15 +1,19 @@
-describe('NuPack', function () {
+describe('NuPack Product Repacking Pricing System', function () {
   var costOfJob = require('../lib/Nupack');
 
-  it('calculates markup price of product #1', function () {
-    expect(costOfJob(1299.99, 3, "food")).toEqual(1591.58);
+  describe('Markup Calculator that takes 3 inputs', function () {
+
+    it('calculates markup price of product #1', function () {
+      expect(costOfJob(1299.99, 3, "food")).toEqual(1591.58);
+    });
+
+    it('calculates markup price of product #2', function () {
+      expect(costOfJob(5432, 1, "drugs")).toEqual(6199.81);
+    });
+
+    it('calculates markup price of product #3', function () {
+      expect(costOfJob(12456.95, 4, "books")).toEqual(13707.63);
+    })
   });
 
-  it('calculates markup price of product #2', function () {
-    expect(costOfJob(5432, 1, "drugs")).toEqual(6199.81);
-  });
-
-  it('calculates markup price of product #3', function () {
-    expect(costOfJob(12456.95, 4, "books")).toEqual(13707.62778);
-  });
 });
